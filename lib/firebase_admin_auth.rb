778b3db2efa7f57415c255b6cf3a9ac747c9f9f7
TaneiKanta <hetaleotu@gmail.com>
2021-04-16 18:31:42 +0900

    initial commit

---
+++ lib/firebase_admin_auth.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "firebase_admin_auth/version"
+
+module FirebaseAdminAuth
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/firebase_admin_auth_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe FirebaseAdminAuth do
+  it "has a version number" do
+    expect(FirebaseAdminAuth::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
